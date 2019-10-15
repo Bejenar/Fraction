74af711871a2eb38ee5225b482093320026133be
AdMyNyRaL AdMyNyRaL <jion93@mail.ru>
2019-10-15 19:50:46 +0300

    changed to static class

--- src/action/FractionAction.java
+++ src/action/FractionAction.java
@@ -6,8 +6,8 @@ import bean.Fraction;
 import calculus.FractionCalc;
 
 public class FractionAction {
-	Scanner scan = new Scanner(System.in);
-		public Fraction[] createFractionArray(int n) {
+	static Scanner scan = new Scanner(System.in);
+		public static Fraction[] createFractionArray(int n) {
 			
 			Fraction a[] = new Fraction[n];
 			for(int i = 0; i < n; i++)
@@ -18,11 +18,11 @@ public class FractionAction {
 			return a;
 		}
 		
-		public void showFractionArray(Fraction [] a) {
+		public static void showFractionArray(Fraction [] a) {
 			System.out.println(Arrays.toString(a));
 		}
 		
-		public void task(Fraction [] a) {
+		public static void task(Fraction [] a) {
 			for(int i = 0; i < a.length; i++) {
 				if ( i % 2 == 0)
 					if (i < a.length - 1)
